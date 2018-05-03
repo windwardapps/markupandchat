3364d109a150aacd4b45dcc7293ddf95e1cb07ab
kyle <kyle@evolphin.com>
2018-05-02 16:13:24 -1000

    Remove serviceworker

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 import './index.css';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
